ee4d2cfdb7e89ae985c89ac8c9af760d694ebf86
Pranay-SPL <pranay.bhujbal@springuplabs.com>
2026-08-26 16:53:21 +0530

    Create hello.bat

---
+++ hello.bat
@@ -0,0 +1,12 @@
+@echo off
+
+echo ==========================
+echo Hello from Jenkins Demo
+echo ==========================
+echo.
+echo Jenkins successfully downloaded this file from GitHub!
+echo.
+echo Current Date:
+date /t
+echo Current Time:
+time /t
